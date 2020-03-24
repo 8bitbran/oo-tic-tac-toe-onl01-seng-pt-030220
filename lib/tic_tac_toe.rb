28f2e58263674d029cf4e056a204888822f485cc
Brandon Marrero <brandonmar97@gmail.com>
2020-03-24 21:07:08 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -116,8 +116,13 @@ class TicTacToe
     index = won? 
     if index == false
       return nil
-      
-      
+    else 
+      if @board[[index[0]] == "X"
+        return "X"
+      else 
+        return "O"
+      end
+    end
   end 
   
 end 
