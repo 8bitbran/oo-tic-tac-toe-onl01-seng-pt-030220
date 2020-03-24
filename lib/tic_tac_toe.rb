89ab80007990f89832d7364ef2583f04b4962d7f
Brandon Marrero <brandonmar97@gmail.com>
2020-03-24 19:47:29 +0000

    Automatically backed up by Learn

---
+++ lib/tic_tac_toe.rb
@@ -0,0 +1,10 @@
+class TicTacToe 
+  
+  WIN_COMBINATIONS = [0, 1, 2],
+                    [3, 4, 5]
+  
+  def initialize
+    @board = [" ", " ", " ", " ", " ", " ", " ", " ", " ",]
+  end 
+  
+end 
